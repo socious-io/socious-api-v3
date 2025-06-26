a53f1eaca64238c6e2c8f6a3e25b14b5880b1af3
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-06-26 19:39:36 +0330

    fix: fixing city and country sending null for upsert

--- src/sql/users/upsert.sql
+++ src/sql/users/upsert.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (id, first_name, last_name, username, email, city, country, avatar, cover_image, language, impact_points, identity_verified) 
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
 ON CONFLICT (id) DO UPDATE SET
-    first_name = EXCLUDED.first_name,
+    first_name = COALESCE(EXCLUDED.first_name,
     last_name = EXCLUDED.last_name,
     username = EXCLUDED.username,
-    city = EXCLUDED.city,
-    country = EXCLUDED.country,
+    city = COALESCE(EXCLUDED.city, city),
+    country = COALESCE(EXCLUDED.country, country),
     avatar = EXCLUDED.avatar,
     cover_image = EXCLUDED.cover_image,
     language = EXCLUDED.language,
